879d16f39626ee24190053ac3b9fa36a4495172c
James Allen <james@sharelatex.com>
2014-12-24 18:50:23 +0000

    Show logged in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def current_user
-    if !@current_user.nil?
-      return @current_user
-    elsif session.has_key?(:user_id)
-      @current_user = User.find_by_id(session[:user_id])
-      return @current_user
-    else
-      return nil
-    end
-  end
-  
-  def logged_in?
-    return !!current_user
-  end
-  
   def return_to_url
     url = session[:return_to] || root_url
     session.delete(:return_to)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,19 @@ module ApplicationHelper
   def twitter_login_path
     "/auth/twitter"
   end
+  
+  def current_user
+    if !@current_user.nil?
+      return @current_user
+    elsif session.has_key?(:user_id)
+      @current_user = User.find_by_id(session[:user_id])
+      return @current_user
+    else
+      return nil
+    end
+  end
+  
+  def logged_in?
+    return !!current_user
+  end
 end
