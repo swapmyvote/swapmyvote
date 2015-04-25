6705534017c7421656dee018a7e38886ba521568
James Allen <james@sharelatex.com>
2015-04-25 16:15:27 +0100

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'faq', to: "static_pages#faq"
   get 'about', to: "static_pages#about"
   get 'privacy', to: "static_pages#privacy"
+  get 'contact', to: "static_pages#contact"
 
   root 'home#index'
   
