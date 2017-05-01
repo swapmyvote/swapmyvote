cdf0d2e8044aa30086e705cfaa9d4980818a48cd
James Allen <james@sharelatex.com>
2017-05-01 17:59:39 +0100

    Use Mailgun for emails

--- config/initializers/actionmailer.rb
+++ config/initializers/actionmailer.rb
@@ -1,10 +1,10 @@
-if ENV['MANDRILL_APIKEY']
+if ENV['MAILGUN_API_KEY']
 	ActionMailer::Base.smtp_settings = {
-		:port =>           '587',
-		:address =>        'smtp.mandrillapp.com',
-		:user_name =>      ENV['MANDRILL_USERNAME'],
-		:password =>       ENV['MANDRILL_APIKEY'],
-		:domain =>         'heroku.com',
+		:port =>           ENV['MAILGUN_SMTP_PORT'],
+		:address =>        ENV['MAILGUN_SMTP_SERVER'],
+		:user_name =>      ENV['MAILGUN_SMTP_LOGIN'],
+		:password =>       ENV['MAILGUN_SMTP_PASSWORD'],
+		:domain =>         ENV['MAILGUN_DOMAIN'],
 		:authentication => :plain
 	}
 	ActionMailer::Base.delivery_method = :smtp
