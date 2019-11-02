28069757764e1dc8923348dfb641cd6b8ae5d85f
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-02 18:12:50 +0000

    Fix omniauth issue
    https://medium.com/@isabelle.giuong/if-you-get-the-error-undefined-local-variable-or-method-env-for-the-line-about-6e5a32dbd875
    
    Must have been introduced during Rails upgrade.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env['omniauth.auth'])
+    user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
     redirect_to return_to_url
   end
