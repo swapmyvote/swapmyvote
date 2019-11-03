e9e3d089f75e23c907c7fc686050f96e9041d199
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-03 20:22:03 +0000

    improve creation of fake users
    - Make firstname gender consistent with image
    - Provide swaps in all directions for the first three parties
    - Refactor to avoid duplicate code

--- db/seeds/development/users.seeds.rb
+++ db/seeds/development/users.seeds.rb
@@ -1,36 +1,25 @@
-10.times do |i|
-  firstname = Random.firstname
+def create_random_user(i, preferred_party_id, willing_party_id)
+  gender = rand > 0.5 ? 'female' : 'male'
+  firstname = gender == 'male' ? Random.firstname_male : Random.firstname_female
+
   User.create(
     name: "#{firstname} #{Random.lastname}",
     email: "#{firstname.downcase}@example.com",
     constituency_id: rand(1 + Constituency.count()),
-    preferred_party_id: 1,
-    willing_party_id: 2,
-    image: "http://api.randomuser.me/portraits/med/men/#{i}.jpg"
+    preferred_party_id: preferred_party_id,
+    willing_party_id: willing_party_id,
+    image: "http://api.randomuser.me/portraits/med/%s/#{i}.jpg" %
+           (gender == 'male' ? 'men' : 'women')
   )
 end
 
-10.times do |i|
-  firstname = Random.firstname
-  User.create(
-    name: "#{firstname} #{Random.lastname}",
-    email: "#{firstname.downcase}@example.com",
-    constituency_id: rand(1 + Constituency.count()),
-    preferred_party_id: 1,
-    willing_party_id: 3,
-    image: "http://api.randomuser.me/portraits/med/men/#{i}.jpg"
-  )
-end
+5.times do |i|
+  create_random_user(i, 1, 2)
+  create_random_user(i, 1, 3)
 
+  create_random_user(i, 2, 1)
+  create_random_user(i, 2, 3)
 
-10.times do |i|
-  firstname = Random.firstname
-  User.create(
-    name: "#{firstname} #{Random.lastname}",
-    email: "#{firstname.downcase}@example.com",
-    constituency_id: rand(1 + Constituency.count()),
-    preferred_party_id: 2,
-    willing_party_id: 3,
-    image: "http://api.randomuser.me/portraits/med/men/#{i}.jpg"
-  )
+  create_random_user(i, 3, 1)
+  create_random_user(i, 3, 2)
 end
