2fd6fe863ed4b7e901403b8afcd33e7baed6bdd5
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-03 20:20:25 +0000

    switch to Rails 5.2 defaults
    The only change significantly affecting us is the change of sqlite3
    booleans from 'f' / 't' to 0 / 1.  This is annoyingly tricky to do
    in sqlite3 (there's no ALTER COLUMN), so provide some example SQL
    in case anyone needs to migrate while preserving their dev db.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Swapmyvote
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     config.action_mailer.default_url_options = {host: "swapmyvote.uk"}
     # Settings in config/environments/* take precedence over those specified here.
---
+++ db/migrate/bool-to-int.sql
@@ -0,0 +1,10 @@
+BEGIN TRANSACTION;
+CREATE TEMPORARY TABLE users_new("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "provider" varchar, "uid" varchar, "name" varchar, "email" varchar, "image" varchar, "token" varchar, "expires_at" datetime, "preferred_party_id" integer, "willing_party_id" integer, "constituency_id" integer, "swap_id" integer, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, "has_voted" boolean DEFAULT '0', "sent_vote_reminder_email" boolean DEFAULT '0');
+INSERT INTO users_new SELECT "id", "provider", "uid", "name", "email", "image", "token", "expires_at", "preferred_party_id", "willing_party_id", "constituency_id", "swap_id", "created_at", "updated_at", "has_voted", "sent_vote_reminder_email" FROM users;
+UPDATE users_new SET has_voted='0' WHERE has_voted='f';
+UPDATE users_new SET has_voted='1' WHERE has_voted='t';
+UPDATE users_new SET sent_vote_reminder_email='0' WHERE sent_vote_reminder_email='f';
+UPDATE users_new SET sent_vote_reminder_email='1' WHERE sent_vote_reminder_email='t';
+DROP TABLE users;
+ALTER TABLE users_new RENAME TO users;
+COMMIT;
