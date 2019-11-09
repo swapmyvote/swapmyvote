swapmyvote/swapmyvote

commit b7b4cc1b1ac898623026a773edcf23cfd67e5d47

[rubocop] fix Style/GuardClause (#57)
Partially addresses #57.

app/controllers/application_controller.rb
@@ -18,22 +18,22 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
-    if !logged_in?
-      if params[:log_in_with]
-        session[:return_to] = url_except_param(request.original_url, :log_in_with)
-        logger.debug "After login will return to #{session[:return_to]}"
-        redirect_to root_path(log_in_with: params[:log_in_with])
-      else
-        redirect_to root_path
-      end
+    if logged_in?
+      @user = current_user
       return
     end
-    @user = current_user
+
+    if params[:log_in_with]
+      session[:return_to] = url_except_param(request.original_url, :log_in_with)
+      logger.debug "After login will return to #{session[:return_to]}"
+      redirect_to root_path(log_in_with: params[:log_in_with])
+    else
+      redirect_to root_path
+    end
   end
 
   def require_swapping_open
-    if !swapping_open?
-      redirect_to root_path
-    end
+    return unless swapping_open?
+    redirect_to root_path
   end
 end

app/controllers/home_controller.rb
@@ -4,15 +4,16 @@ class HomeController < ApplicationController
       redirect_to user_path
       return
     end
+
+    return if session["pre_populate"].nil?
+
     @parties = Party.all
-    if not session["pre_populate"].nil?
-      for party in @parties
-        if canonical_name(party.name) == canonical_name(session["pre_populate"]["preferred_party_name"])
-          @preferred_party_id = party.id
-        end
-        if canonical_name(party.name) == canonical_name(session["pre_populate"]["willing_party_name"])
-          @willing_party_id = party.id
-        end
+    for party in @parties
+      if canonical_name(party.name) == canonical_name(session["pre_populate"]["preferred_party_name"])
+        @preferred_party_id = party.id
+      end
+      if canonical_name(party.name) == canonical_name(session["pre_populate"]["willing_party_name"])
+        @willing_party_id = party.id
       end
     end
   end

app/controllers/user/swaps_controller.rb
@@ -38,16 +38,14 @@ class User::SwapsController < ApplicationController
 
 private
   def assert_incoming_swap_exists
-    if !@user.incoming_swap
-      flash[:errors] = ["You don't have a swap!"]
-      redirect_to user_path
-    end
+    return if @user.incoming_swap
+    flash[:errors] = ["You don't have a swap!"]
+    redirect_to user_path
   end
 
   def assert_parties_exist
-    if !@user.willing_party || !@user.preferred_party
-      redirect_to edit_user_path
-    end
+    return if @user.willing_party and @user.preferred_party
+    redirect_to edit_user_path
   end
 
   def swap_params

app/controllers/user/vote_controller.rb
@@ -14,8 +14,7 @@ class User::VoteController < ApplicationController
 
 private
   def require_swap
-    if !@user.is_swapped?
-      redirect_to user_path
-    end
+    return if @user.is_swapped?
+    redirect_to user_path
   end
 end

app/controllers/users_controller.rb
@@ -5,10 +5,9 @@ class UsersController < ApplicationController
   def new
     @user_params = session[:user_params] || {}
     session.delete(:user_params)
-    if !@user_params.has_key?("preferred_party_id") || !@user_params.has_key?("willing_party_id")
-      redirect_to root_path
-      return
-    end
+    return if (@user_params.has_key?("preferred_party_id") &&
+               @user_params.has_key?("willing_party_id"))
+    redirect_to root_path
   end
 
   def create
@@ -51,11 +50,11 @@ private
   # it into the session here.
   def require_login_and_save_user_params
     logged_in = require_login()
-    if !logged_in
-      # Set params to resume user creation after login
-      session[:user_params] = user_params
-      session[:return_to] = new_user_path
-    end
+    return if logged_in
+
+    # Set params to resume user creation after login
+    session[:user_params] = user_params
+    session[:return_to] = new_user_path
   end
 
   def user_params

app/helpers/application_helper.rb
@@ -7,14 +7,12 @@ module ApplicationHelper
   end
 
   def current_user
-    if !@current_user.nil?
-      return @current_user
-    elsif session.has_key?(:user_id)
-      @current_user = User.find_by_id(session[:user_id])
-      return @current_user
-    else
-      return nil
-    end
+    return @current_user if !@current_user.nil?
+
+    return nil unless session.has_key?(:user_id)
+
+    @current_user = User.find_by_id(session[:user_id])
+    return @current_user
   end
 
   def logged_in?

app/models/user.rb
@@ -31,13 +31,13 @@ class User < ApplicationRecord
   end
 
   def profile_url
-    if provider == "facebook"
-      return "https://facebook.com/#{uid}"
-    elsif provider == "twitter"
-      return "https://twitter.com/intent/user?user_id=#{uid}"
-    else
-      return "#"
-    end
+    return "https://facebook.com/#{uid}" \
+      if provider == "facebook"
+
+    return "https://twitter.com/intent/user?user_id=#{uid}" \
+      if provider == "twitter"
+
+    return "#"
   end
 
   def image_url
@@ -106,13 +106,9 @@ class User < ApplicationRecord
   end
 
   def swapped_with
-    if outgoing_swap
-      return outgoing_swap.chosen_user
-    elsif incoming_swap
-      return incoming_swap.choosing_user
-    else
-      return nil
-    end
+    return outgoing_swap.chosen_user if outgoing_swap
+    return incoming_swap.choosing_user if incoming_swap
+    return nil
   end
 
   def is_swapped?

commit c3825ce7a3c873feee3a7efc0d2ba9f6185dcadf

Fix redirect loop caused by b7b4cc1b1ac89 in #69
Oops.  How I wish we had proper tests...

app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_swapping_open
-    return unless swapping_open?
+    return if swapping_open?
     redirect_to root_path
   end
 end