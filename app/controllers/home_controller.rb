6d7eb0e0de5d488b63489dde3e63af8baaba3dee
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-09 16:22:09 +0000

    Fix breakage introduced fixing rubocop Style/GuardClause (#57)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,10 @@ class HomeController < ApplicationController
       return
     end
 
+    @parties = Party.all
+
     return if session["pre_populate"].nil?
 
-    @parties = Party.all
     @parties.each do |party|
       if canonical_name(party.name) == canonical_name(session["pre_populate"]["preferred_party_name"])
         @preferred_party_id = party.id
