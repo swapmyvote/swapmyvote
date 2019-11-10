swapmyvote/swapmyvote

commit 90440b2bd60d093398b83dac070e067df82cdcd9

mitigate CVE-2015-9284
Based on advice from https://github.com/omniauth/omniauth/wiki/Resolving-CVE-2015-9284

- Add omniauth-rails_csrf_protection gem
- Change GET /auth/:provider to POST

Fixes #19

app/controllers/application_controller.rb
@@ -11,13 +11,18 @@ class ApplicationController < ActionController::Base
     return url
   end
 
+  def url_except_param(url, param)
+    uri = Addressable::URI.parse(url)
+    uri.query_values = uri.query_values.except(param.to_s)
+    return uri.to_s.chomp("?")
+  end
+
   def require_login
     if !logged_in?
-      session[:return_to] = request.original_url
-      if params[:log_in_with] == "twitter"
-        redirect_to twitter_login_path
-      elsif params[:log_in_with] == "facebook"
-        redirect_to facebook_login_path
+      if params[:log_in_with]
+        session[:return_to] = url_except_param(request.original_url, :log_in_with)
+        logger.debug "After login will return to #{session[:return_to]}"
+        redirect_to root_path(log_in_with: params[:log_in_with])
       else
         redirect_to root_path
       end

app/views/home/index.html.haml
@@ -51,12 +51,14 @@
       .modal-dialog
         %i.fa.fa-times.subdued.modal-close
         %p.text-center
-          %button.button.button-facebook{type: "submit", name: "log_in_with", value: "facebook"}
+          = link_to facebook_login_path, class: "button button-facebook",
+                    method: :post do
             %i.fa.fa-fw.fa-facebook
             Log in with Facebook
         %p.text-center.subdued &#8212; or &#8212;
         %p.text-center
-          %button.button.button-twitter{type: "submit", name: "log_in_with", value: "twitter"}
+          = link_to twitter_login_path, class: "button button-twitter",
+                    method: :post do
             %i.fa.fa-fw.fa-twitter
             Log in with Twitter
         %p.text-center.small.subdued

app/views/layouts/application.html.haml
@@ -45,18 +45,23 @@
       - else
         %span.user
           = link_to "Already been here? Log in", "#", class: "small subdued logout", onclick: "showModal($('.js-login-modal')); return false"
-        .modal.js-login-modal{style: "display: none;"}
+        .modal.js-login-modal{style: "display: %s;" % (params[:log_in_with] ? 'inherit' : 'none')}
           .modal-dialog
             %i.fa.fa-times.subdued.modal-close
-            %p.text-center
-              = link_to facebook_login_path, class: "button button-facebook" do
-                %i.fa.fa-fw.fa-facebook
-                Log in with Facebook
-            %p.text-center.subdued &#8212; or &#8212;
-            %p.text-center
-              = link_to twitter_login_path, class: "button button-twitter" do
-                %i.fa.fa-fw.fa-twitter
-                Log in with Twitter
+            - if !params[:log_in_with] or params[:log_in_with] == 'facebook'
+              %p.text-center
+                = link_to facebook_login_path, class: "button button-facebook",
+                          method: :post do
+                  %i.fa.fa-fw.fa-facebook
+                  Log in with Facebook
+            - if !params[:log_in_with]
+              %p.text-center.subdued &#8212; or &#8212;
+            - if !params[:log_in_with] or params[:log_in_with] == 'twitter'
+              %p.text-center
+                = link_to twitter_login_path, class: "button button-twitter",
+                          method: :post do
+                  %i.fa.fa-fw.fa-twitter
+                  Log in with Twitter
             %p.text-center.small.subdued
               (We won't share anything with anyone about your voting preferences <br/> or post to your feed without your permission)
             %p.text-center

Gemfile
@@ -34,9 +34,10 @@ gem 'jquery-rails'
 
 gem 'omniauth-facebook'
 gem 'omniauth-twitter'
+gem 'omniauth-rails_csrf_protection', '~> 0.1'
 
 gem 'haml-rails'
-
+gem 'addressable'
 gem 'normalize-rails'
 
 gem 'seedbank'

commit 4f2ed7d31bbb3084cfce2498484bee31e4874ba3

Fix breakage introduced by mitigation of CVE-2015-9284 (#41, #42)
90440b2bd60d broke saving of the user's swap preferences on login,
since an <a method="post" ...> element inside a <form> does not
include the other parameters within the <form> when submitting the
action via POST.  So replace it with a <button> element which does.
Also, since we want a single login modal which can work from any page
on the site, move the <form> into application.html.haml, give it an
id="login-form" attribute, and give the <select> elements a
form="login-form" attribute so that their values get submitted to it
on login.

However since we now have to POST directly to /auth/:provider in order
to mitigate CVE-2015-9284, we need some omniauth trickery to capture
the user's voting preferences before that POST, in lieu of the
existing require_login_and_save_user_params filter.  (The latter
previously worked because we used to initiate login via POST to our
own user_path.)

The new flow is:

- POST /auth/:provider, potentially including parameters
  detailing user's voting preferences

- omniauth before_request_phase handler saves those user preferences
  in the rack session (env["rack.session"])

- omniauth request phase calls the identity provider and
  (assuming success) gets identity information back

- omniauth callback phase calls session#create

- session#create gets or creates a User object, updates it with any
  user preferences retrieved from the session object, and redirects to
  whichever page the login request originated from, or user#show by
  default.

Fixes #41
Fixes #42
See also #36 although that is not fixed yet

app/controllers/application_controller.rb
@@ -5,12 +5,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def return_to_url
-    url = session[:return_to] || root_url
-    session.delete(:return_to)
-    return url
-  end
-
   def url_except_param(url, param)
     uri = Addressable::URI.parse(url)
     uri.query_values = uri.query_values.except(param.to_s)

app/controllers/sessions_controller.rb
@@ -2,7 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to return_to_url
+    user_params = session.delete(:user_params)
+    user.update(user_params) if user_params
+    origin = request.env["omniauth.origin"]
+    logger.debug "Login request origin #{origin}"
+    redirect_to origin || user_path
   end
 
   def retry

app/controllers/users_controller.rb
@@ -1,19 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login_and_save_user_params, only: :create
-  before_action :require_login, except: :create
-
-  def new
-    @user_params = session[:user_params] || {}
-    session.delete(:user_params)
-    return if @user_params.key?("preferred_party_id") &&
-              @user_params.key?("willing_party_id")
-    redirect_to root_path
-  end
-
-  def create
-    @user.update(user_params)
-    redirect_to user_path
-  end
+  before_action :require_login
 
   def show
     if !@user.constituency || !@user.email
@@ -27,6 +13,7 @@ class UsersController < ApplicationController
     end
   end
 
+  # Users can get here from the "Not right? Update your info" link
   def edit
     @parties = Party.all
     @constituencies = Constituency.all.order(:name)
@@ -44,19 +31,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  # The home page sends us to user/create, but if we're not logged in
-  # we need to first divert and do that, then come back to the user/new page
-  # with the constituency still populated. To keep that data around, we save
-  # it into the session here.
-  def require_login_and_save_user_params
-    logged_in = require_login
-    return if logged_in
-
-    # Set params to resume user creation after login
-    session[:user_params] = user_params
-    session[:return_to] = new_user_path
-  end
 
   def user_params
     params.require(:user).permit(:preferred_party_id, :willing_party_id, :constituency_id, :email)

app/views/home/index.html.haml
@@ -1,59 +1,58 @@
 - if swapping_open?
-  = form_for :user, url: user_path do |f|
-    .background-pattern.border-bottom
-      .container.text-center
-        %h1
-          Make your vote count on 12 Dec 2019!
-        %h2.subdued
-          Swap it with someone in a constituency where it could count for more. You get to vote for who you really want, and to help someone else do the same.
+  .background-pattern.border-bottom
+    .container.text-center
+      %h1
+        Make your vote count on 12 Dec 2019!
+      %h2.subdued
+        Swap it with someone in a constituency where it could count for more. You get to vote for who you really want, and to help someone else do the same.
 
-    .plain-pattern.border-bottom
-      .container.text-center
-        %p.choose_party
-          My preferred party is
-          = select :user, :preferred_party_id do
-            %option{disabled: true, selected: true} ...
-            = options_for_select(@parties.map {|p| [ p.name, p.id ]}, selected: @preferred_party_id)
-        %p.choose_party
-          but I'm willing to vote for
-          = select :user, :willing_party_id do
-            %option{disabled: true, selected: true} ...
-            = options_for_select(@parties.map {|p| [ p.name, p.id ]}, selected: @willing_party_id)
-        %p
-          %a.button.button-large{onclick: "submit_modal(); return false", href: "#"} Swap My Vote!
-        %p.small.subdued
-          We will match you with someone who will
-          cast your preferred vote in a different area where it could count for more.
-          In return, you will cast their preferred vote in your area.
+  .plain-pattern.border-bottom
+    .container.text-center
+      %p.choose_party
+        My preferred party is
+        = select :user, :preferred_party_id, nil, {}, {form: 'login-form'} do
+          %option(disabled=true selected=true) ...
+          = options_for_select(@parties.map {|p| [ p.name, p.id ]}, selected: @preferred_party_id)
+      %p.choose_party
+        but I'm willing to vote for
+        = select :user, :willing_party_id, nil, {}, {form: 'login-form'} do
+          %option(disabled=true selected=true) ...
+          = options_for_select(@parties.map {|p| [ p.name, p.id ]}, selected: @willing_party_id)
+      %p
+        %a.button.button-large{onclick: "submit_modal(); return false", href: "#"} Swap My Vote!
+      %p.small.subdued
+        We will match you with someone who will
+        cast your preferred vote in a different area where it could count for more.
+        In return, you will cast their preferred vote in your area.
 
-    .background-pattern.border-bottom
-      .container
-        %h2 How does Swap My Vote work?
+  .background-pattern.border-bottom
+    .container
+      %h2 How does Swap My Vote work?
 
-        %ol
-          %li Choose the party you would ideally vote for, and the party that you are willing to vote for tactically in your local constituency.
+      %ol
+        %li Choose the party you would ideally vote for, and the party that you are willing to vote for tactically in your local constituency.
 
-          %li We’ll find you a list of people with the complementary preferences. Pick one partner to swap your vote with. The recent polls by their name can help you see where your vote might make most difference.
+        %li We’ll find you a list of people with the complementary preferences. Pick one partner to swap your vote with. The recent polls by their name can help you see where your vote might make most difference.
 
-          %li If your partner agrees to the swap, it is confirmed. We’ll put you in touch with each other’s Facebook or Twitter profiles and, if you like, you can introduce yourselves.
-    :javascript
-      function submit_modal() {
-        preferred_party_id = $("select[name='user[preferred_party_id]']").val();
-        willing_party_id = $("select[name='user[willing_party_id]']").val();
-        if (preferred_party_id !== null && willing_party_id !== null) {
-          showModal($('.js-login-modal'));
-        } else {
-          showModal($('.js-error-modal'));
-        }
+        %li If your partner agrees to the swap, it is confirmed. We’ll put you in touch with each other’s Facebook or Twitter profiles and, if you like, you can introduce yourselves.
+  :javascript
+    function submit_modal() {
+      preferred_party_id = $("select[name='user[preferred_party_id]']").val();
+      willing_party_id = $("select[name='user[willing_party_id]']").val();
+      if (preferred_party_id !== null && willing_party_id !== null) {
+        showModal($('.js-login-modal'));
+      } else {
+        showModal($('.js-error-modal'));
       }
+    }
 
-    .modal.js-error-modal{style: "display: none;"}
-      .modal-dialog
-        %i.fa.fa-times.subdued.modal-close
-        %p.text-center
-          Please choose both your preferred party and your willing party.
-        %p.text-center
-          %a.button.modal-close{href: "#"} OK
+  .modal.js-error-modal{style: "display: none;"}
+    .modal-dialog
+      %i.fa.fa-times.subdued.modal-close
+      %p.text-center
+        Please choose both your preferred party and your willing party.
+      %p.text-center
+        %a.button.modal-close{href: "#"} OK
 
 - else
   .background-pattern.border-bottom

app/views/layouts/application.html.haml
@@ -46,20 +46,21 @@
         %span.user
           = link_to "Already been here? Log in", "#", class: "small subdued logout", onclick: "showModal($('.js-login-modal')); return false"
         .modal.js-login-modal{style: "display: %s;" % (params[:log_in_with] ? 'inherit' : 'none')}
-          .modal-dialog
+          = form_with url: "#", id: "login-form", class: "modal-dialog",
+                      local: true do
             %i.fa.fa-times.subdued.modal-close
             - if !params[:log_in_with] or params[:log_in_with] == 'facebook'
               %p.text-center
-                = link_to facebook_login_path, class: "button button-facebook",
-                          method: :post do
+                %button{formaction: facebook_login_path, formmethod: :post,
+                        class: "button button-facebook"}
                   %i.fa.fa-fw.fa-facebook
                   Log in with Facebook
             - if !params[:log_in_with]
               %p.text-center.subdued &#8212; or &#8212;
             - if !params[:log_in_with] or params[:log_in_with] == 'twitter'
               %p.text-center
-                = link_to twitter_login_path, class: "button button-twitter",
-                          method: :post do
+                %button{formaction: twitter_login_path, formmethod: :post,
+                        class: "button button-twitter"}
                   %i.fa.fa-fw.fa-twitter
                   Log in with Twitter
             %p.text-center.small.subdued

app/views/users/new.html.haml
@@ -1,11 +1,0 @@
-.container
-  %p.text-center Saving...
-
-= form_for :user, url: user_path do |f|
-  = hidden_field :user, :preferred_party_id, value: @user_params["preferred_party_id"]
-  = hidden_field :user, :willing_party_id, value: @user_params["willing_party_id"]
-
-:javascript
-  $(document).ready(function(){
-    $("form").submit();
-  });

config/initializers/omniauth.rb
@@ -1,4 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+           origin_param: 'return_to'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],
+           origin_param: 'return_to'
+
+  before_request_phase do |env|
+    # Prior to login, save user parameters to session so that we can
+    # retrieve them after authentication in order to update the user's
+    # preferences.
+    #
+    # This API is basically undocumented and was figured from reading
+    # omniauth's strategy.rb.
+    env["rack.session"]["user_params"] = env["rack.request.form_hash"]["user"]
+  end
 end