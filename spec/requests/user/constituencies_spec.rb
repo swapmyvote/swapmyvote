c3c647bf407d50c52dbfb0d8f1116ef77b891e78
baob <coder@onesandthrees.com>
2019-11-15 14:09:58 +0000

    request: get corrected to path from: rails routes (#8)

--- spec/requests/user/constituencies_spec.rb
+++ spec/requests/user/constituencies_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "User::Constituencies", type: :request do
-  describe "GET /user/constituencies" do
+  describe "GET /user/constituencies/edit" do
     it "works! (now write some real specs)" do
-      get user_constituencies_index_path
+      get edit_user_constituency_path
       expect(response).to have_http_status(200)
     end
   end
