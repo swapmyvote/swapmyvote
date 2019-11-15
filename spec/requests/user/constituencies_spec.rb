639092825be7123b778de9913a2d25e5965408cf
baob <coder@onesandthrees.com>
2019-11-15 14:06:07 +0000

    spec as genned by: rails generate rspec:request user/constituencies (#8)

---
+++ spec/requests/user/constituencies_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "User::Constituencies", type: :request do
+  describe "GET /user/constituencies" do
+    it "works! (now write some real specs)" do
+      get user_constituencies_index_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
