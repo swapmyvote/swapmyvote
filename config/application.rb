1fb200154fc1f9f30acac69019cfa31729f9012d
Adam Spiers <git@adamspiers.org>
2019-11-16 13:07:49 +0400

    separate SERVER_HOST from port so we can choose a cookie name correctly
    Previously we were only using SERVER_HOST to construct URLs in emails
    we send.  Now we want to use it to decide the right cookie name, so
    store the port number (if needed) in a separate variable.
    
    Fixes #134.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SwapMyVote
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.action_mailer.default_url_options = { host: ENV["SERVER_HOST"] }
+    config.action_mailer.default_url_options = { host: ENV["SERVER_HOST_PORT"] }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
