54fc54abc7f0ede87a26afef3a1f7399b2533fc6
baob <coder@onesandthrees.com>
2019-11-18 22:22:05 +0000

    User#constituency unit test (#8)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,23 @@ RSpec.describe User, type: :model do
       expect(subject.image_url).to eq("https://facebook.com/image/1234.jpg")
     end
   end
+
+  describe "#constituency" do
+    context 'with user with no constituency id' do
+      let(:no_constituency_user) { User.new(name: 'fred') }
+
+      it 'is nil' do
+        expect(no_constituency_user.constituency).to be_nil
+      end
+    end
+
+    context 'with user with constituency id' do
+      let(:constituency) { Constituency.create!(name: 'test con 1') }
+      let(:user) { User.new(name: 'test user', constituency_id: constituency.id) }
+
+      it 'is expected constituency' do
+        expect(user.constituency).to eq(constituency)
+      end
+    end
+  end
 end
