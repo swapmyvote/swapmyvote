a08425dee89f66f5fea623fe66ef33c5122d86ef
egglestn <segglestn@gmail.com>
2019-11-18 15:09:30 +0530

    First draft of privacy policy page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'faq', to: "static_pages#faq"
   get 'about', to: "static_pages#about"
   get 'privacy', to: "static_pages#privacy"
+  get 'cookies', to: "static_pages#cookies"
+  get 'terms', to: "static_pages#terms"
   get 'contact', to: "static_pages#contact"
   get 'account_deleted', to: "static_pages#account_deleted"
   get 'confirm_account_deletion', to: "static_pages#confirm_account_deletion"
