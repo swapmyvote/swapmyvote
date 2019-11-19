1134846c223c2dafc3d51501f08c9b4ab1c8d0dd
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-19 14:15:38 +0000

    Upgrade Facebook Graph API to 3.0
    2.10 was just EOL'd; 2.11 and 2.12 are available for a while
    longer but let's try to get a bit ahead of the game.  3.0 will
    EOL on 26th July 2020.
    
    See also https://github.com/mkdynamic/omniauth-facebook/pull/%33%33%32

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           origin_param: 'return_to'
+           origin_param: 'return_to',
+           client_options: {
+             site: 'https://graph.facebook.com/v3.0',
+             authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+           }
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'],
            origin_param: 'return_to'
 
