93a0bf495efd47945566e9db167cf0bf3464d5a4
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-20 17:27:32 +0000

    Add test for uniqueness of mobile_phone number field

--- spec/models/mobile_phone_spec.rb
+++ spec/models/mobile_phone_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe MobilePhone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "prevents two users having the same number" do
+    number = "07771 111 111"
+    subject.user_id = User.create.id
+    subject.number = number
+    subject.save!
+    expect(subject.id).not_to be_nil
+    expect {
+      MobilePhone.create!(number: number)
+    }.to raise_error(ActiveRecord::RecordInvalid,
+                     /Number has already been taken/)
+  end
 end
