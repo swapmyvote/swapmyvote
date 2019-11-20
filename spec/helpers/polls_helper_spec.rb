85f9d96c84f702f1f3ea67dde172e8b45d91d7b9
baob <coder@onesandthrees.com>
2019-11-20 21:20:32 +0000

    fix broken PollsHelper spec (#8)

--- spec/helpers/polls_helper_spec.rb
+++ spec/helpers/polls_helper_spec.rb
@@ -2,7 +2,12 @@ require "rails_helper"
 
 RSpec.describe PollsHelper, type: :helper do
   describe "#poll_data_for" do
-    let(:constituency) { Constituency.new(id: 1) }
+    let(:constituency) { Constituency.new(id: 11) }
+    before do
+      constituency.save!
+      party = Party.create!(id: 22)
+      Poll.create!(constituency_id: constituency.id, party_id: party.id, votes: 385.0)
+    end
 
     specify { expect { helper.poll_data_for(constituency) }.not_to raise_error }
   end
