be020e807ef47b1bc152c5eaea232fbe134c25f5
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-22 22:23:41 +0000

    shorten method to keep rubocop happy

--- app/controllers/mobile_phone_controller.rb
+++ app/controllers/mobile_phone_controller.rb
@@ -11,14 +11,7 @@ class MobilePhoneController < ApplicationController
       return
     end
 
-    if phone.verify_id
-      logger.debug "Deleting previous verify id #{phone.verify_id}"
-      errors = SwapMyVote::MessageBird.verify_delete(phone.verify_id)
-      if errors
-        logger.error("verify_delete(#{phone.verify_id}) failed: " +
-                     errors.join("\n"))
-      end
-    end
+    delete_previous_verify_id if phone.verify_id
 
     phone.verify_id = otp.id
     logger.debug "Created verification for user #{current_user.id} / " \
@@ -50,6 +43,14 @@ class MobilePhoneController < ApplicationController
   def verify_template
     "Your verification code is %token. Please enter this code at " +
       verify_token_url(log_in_with: current_user.provider)
+  end
+
+  def delete_previous_verify_id
+    logger.debug "Deleting previous verify id #{phone.verify_id}"
+    errors = SwapMyVote::MessageBird.verify_delete(phone.verify_id)
+    return unless errors
+    logger.error("verify_delete(#{phone.verify_id}) failed: " +
+                 errors.join("\n"))
   end
 
   def handle_errors(errors, msg)
