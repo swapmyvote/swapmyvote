da226454dfdfdd3c1c858fd466c6c6acfe1a2ad3
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-23 11:19:55 +0000

    refactor HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,12 +10,16 @@ class HomeController < ApplicationController
     return if session["pre_populate"].nil?
 
     @parties.each do |party|
-      if canonical_name(party.name) == canonical_name(session["pre_populate"]["preferred_party_name"])
+      if canonical_name(party.name) == prepopulated_party("preferred_party_name")
         @preferred_party_id = party.id
       end
-      if canonical_name(party.name) == canonical_name(session["pre_populate"]["willing_party_name"])
+      if canonical_name(party.name) == prepopulated_party("willing_party_name")
         @willing_party_id = party.id
       end
     end
+  end
+
+  def prepopulated_party(param)
+    canonical_name(session["pre_populate"][param])
   end
 end
