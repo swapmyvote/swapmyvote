f42494308cbb642b8411f07387088c4b4cfbddda
baob <coder@onesandthrees.com>
2019-11-23 12:16:55 +0000

    add migration to ensure ons_constituencies table is populated (#8)

--- app/models/ons_constituency.rb
+++ app/models/ons_constituency.rb
@@ -1,2 +1,3 @@
 class OnsConstituency < ApplicationRecord
+  NUMBER_OF_UK_CONSTITUENCIES = 650
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_155600) do
+ActiveRecord::Schema.define(version: 2019_11_23_112854) do
 
   create_table "constituencies", force: :cascade do |t|
     t.string "name"
