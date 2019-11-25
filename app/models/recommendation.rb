875dc1f708a3f08832f4040f5155cfb4679ce8d2
baob <coder@onesandthrees.com>
2019-11-25 11:03:25 +0000

    add constituency relation to recommendations (#9)

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,2 +1,6 @@
 class Recommendation < ApplicationRecord
+  belongs_to :constituency,
+             class_name: "OnsConstituency",
+             primary_key: "ons_id",
+             foreign_key: "constituency_ons_id"
 end
--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Recommendation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#constituency" do
+    specify { expect {subject.constituency}.not_to raise_error }
+
+    it "is a relation to OnsConstituency" do
+      expect(described_class.reflections["constituency"].class_name).to eq(OnsConstituency.name)
+    end
+  end
 end
