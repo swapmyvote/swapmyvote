e8c59565b5834224c669c552ae7cab6079fecebc
baob <coder@onesandthrees.com>
2019-11-25 10:23:18 +0000

    migration to populate recommendations table (#9)

---
+++ db/migrate/20191125100635_populate_recommendations.rb
@@ -0,0 +1,18 @@
+require_relative "../fixtures/livefrombrexit_recommendations_json"
+
+class PopulateRecommendations < ActiveRecord::Migration[5.2]
+  def up
+    json = LivefrombrexitRecommendationsJson.new
+
+    json.each do |rec_as_hash|
+      rec = Recommendation.find_or_initialize_by(rec_as_hash.slice("site", "constituency_ons_id"))
+      rec.text = rec_as_hash["recommendation"]
+      rec.link = rec_as_hash["link"]
+      rec.save!
+    end
+  end
+
+  def down
+    # could do a delete ?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_125417) do
+ActiveRecord::Schema.define(version: 2019_11_25_100635) do
 
   create_table "constituencies", force: :cascade do |t|
     t.string "name"
