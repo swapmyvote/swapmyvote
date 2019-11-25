09490cd21f1786530c7acc74cca00a1cf37f7ba4
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-25 09:43:21 +0000

    default to SERVER_HOST if SERVER_HOST_PORT is not set
    This is more backwards compatible.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,9 @@ module SwapMyVote
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.action_mailer.default_url_options = { host: ENV["SERVER_HOST_PORT"] }
+    config.action_mailer.default_url_options = {
+      host: ENV["SERVER_HOST_PORT"] || ENV["SERVER_HOST"]
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
