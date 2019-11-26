3ddf22dd4ad23773467b83f034ede5520d5dd140
Steve Baxter <steve@kuamka.com>
2019-11-26 21:17:35 +0000

    Fixing IE11 support

--- app/javascript/packs/intlTelInput.js
+++ app/javascript/packs/intlTelInput.js
@@ -6,8 +6,9 @@ import "intl-tel-input/build/js/utils.js";
 // Set up all phone number inputs with intl-tel-input. We will add a hidden field
 // that allows forms to submit the full number. We use HTML5 validation.
 $(document).ready(() => {
-  document.querySelectorAll("input[type=tel]").forEach(e => {
-    const intlTelPlugin = intlTelInput(e, {
+  const phoneInputs = document.querySelectorAll("input[type=tel]");
+  Array.from(phoneInputs).forEach(phoneInput => {
+    const intlTelPlugin = intlTelInput(phoneInput, {
       initialCountry: "gb",
       preferredCountries: ["gb"],
       formatOnDisplay: true,
@@ -16,7 +17,7 @@ $(document).ready(() => {
       utilsScript: "utils.js"
     });
 
-    e.onchange = () => {
+    phoneInput.onchange = () => {
       if (intlTelPlugin.isValidNumber()) {
         const numberType = intlTelPlugin.getNumberType();
         if (
@@ -24,13 +25,15 @@ $(document).ready(() => {
           numberType === intlTelInputUtils.numberType.FIXED_LINE_OR_MOBILE
         ) {
           // Looks valid!
-          e.setCustomValidity("");
+          phoneInput.setCustomValidity("");
         } else {
           // Valid but not a mobile number
-          e.setCustomValidity("This doesn't look like a mobile phone number");
+          phoneInput.setCustomValidity(
+            "This doesn't look like a mobile phone number"
+          );
         }
       } else {
-        e.setCustomValidity("This doesn't look like a phone number");
+        phoneInput.setCustomValidity("This doesn't look like a phone number");
       }
     };
   });
