8f654c992e5ba13df9843f8dfcd67ad493e1b2a9
baob <coder@onesandthrees.com>
2019-11-26 17:12:50 +0000

    hacking a polls test

--- spec/helpers/polls_helper_spec.rb
+++ spec/helpers/polls_helper_spec.rb
@@ -1,9 +1,23 @@
 require "rails_helper"
 
 RSpec.describe PollsHelper, type: :helper do
-  describe "#poll_data_for" do
-    let(:constituency) { OnsConstituency.new(id: 1) }
+  # fixtures :ons_constituencies, :polls
 
-    specify { expect { helper.poll_data_for(constituency) }.not_to raise_error }
+  describe "#poll_data_for" do
+    context "with single party, single constituency" do
+      let(:constituency) { build(:ons_constituency) }
+      let(:party) { build(:party, name: "Pink") }
+      let(:poll_1) { build(:poll, party: party, constituency_ons_id: constituency.ons_id, votes: 4234)}
+      before(:each) do
+        constituency.save!
+        poll_1.save!
+      end
+
+      specify { expect { helper.poll_data_for(constituency) }.not_to raise_error }
+
+      it "returns the party data" do
+        expect(helper.poll_data_for(constituency)).to match('"Pink"')
+      end
+    end
   end
 end
