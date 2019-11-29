27e39511c8d83e96ec602bd8922bdfd9e83d2bd3
Adam Spiers <swapmyvote@adamspiers.org>
2019-11-29 19:49:27 +0000

    Wrap postcodesHelper.js in document ready handler
    This appears to be needed at least for the Enter key handling in the
    next commit, and anyway it seems like best practice - seems like a
    good idea to copy approach Steve took, and he did this in
    intlTelInput.js ;-)

--- app/javascript/packs/postcodesHelper.js
+++ app/javascript/packs/postcodesHelper.js
@@ -1,45 +1,47 @@
 // Help with finding constituencies from postcodes
 
-//display results on page
-const displayData = postcode => {
-  var onsId = postcode.result.codes.parliamentary_constituency;
-  var name = postcode.result.parliamentary_constituency;
+$(document).ready(() => {
+  //display results on page
+  const displayData = postcode => {
+    var onsId = postcode.result.codes.parliamentary_constituency;
+    var name = postcode.result.parliamentary_constituency;
 
-  $("select#user_constituency_ons_id")
-    .val(onsId)
-    .change(); // this SHOULD change the dropdown
-  $(".constituency-autocomplete-input").val(name);
-};
+    $("select#user_constituency_ons_id")
+      .val(onsId)
+      .change(); // this SHOULD change the dropdown
+    $(".constituency-autocomplete-input").val(name);
+  };
 
-const postcodeButton = () => {
-  console.log("postcode search button clicked");
-  var input = $("#txt-postcode").val();
-  var url = "https://api.postcodes.io/postcodes/" + input;
-  post(url).done(displayData);
-};
+  const postcodeButton = () => {
+    console.log("postcode search button clicked");
+    var input = $("#txt-postcode").val();
+    var url = "https://api.postcodes.io/postcodes/" + input;
+    post(url).done(displayData);
+  };
 
-const ajaxError = (desc, _status, _err) => {
-  if (desc.status == 404 || desc.status == 400) {
-    $("#error-postcode").html(JSON.parse(desc.responseText).error);
-  } else {
-    $("#error-postcode").html(
-      "Postcode Service Error Details: " + desc.responseText
-    );
-  }
-};
+  const ajaxError = (desc, _status, _err) => {
+    if (desc.status == 404 || desc.status == 400) {
+      $("#error-postcode").html(JSON.parse(desc.responseText).error);
+    } else {
+      $("#error-postcode").html(
+        "Postcode Service Error Details: " + desc.responseText
+      );
+    }
+  };
 
-const ajaxSuccess = () => {
-  $("#error-postcode").html("");
-};
+  const ajaxSuccess = () => {
+    $("#error-postcode").html("");
+  };
 
-//ajax call
-const post = url => {
-  return $.ajax({
-    url: url,
-    success: ajaxSuccess,
-    error: ajaxError
-  });
-};
+  //ajax call
+  const post = url => {
+    return $.ajax({
+      url: url,
+      success: ajaxSuccess,
+      error: ajaxError
+    });
+  };
 
-// button event
-$(document).on("click", "#btn-postcode", postcodeButton);
+  // button event
+  $(document).on("click", "#btn-postcode", postcodeButton);
+});
