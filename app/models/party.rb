43015515253b1ee8d9849af313b0206fc8e17007
baob <coder@onesandthrees.com>
2019-11-30 12:29:02 +0000

    add the fixed party data to the party model
    This is a combination of 5 commits.
    
    pending spec
    add Party.canonical_name
    add party short_codes
    add standard party names
    refactor

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,34 @@
 class Party < ApplicationRecord
   has_many :polls
+
+  RAW_MASTER_LIST = [
+    { name: "Labour", color: "#DC241f", short_code: :lab },
+    { name: "Liberal Democrats", color: "#FFB602", short_code: :libdem },
+    { name: "Green Party", color: "#6AB023", short_code: :green },
+    { name: "Conservatives", color: "#0087DC", short_code: :con },
+    { name: "UKIP", color: "#70147A", short_code: :ukip },
+    { name: "SNP", color: "#FFF95D", short_code: :snp },
+    { name: "Plaid Cymru", color: "#008142", short_code: :plaid },
+    { name: "Brexit Party", color: "#5bc0de", short_code: :brexit }
+  ]
+
+  class << self
+    def canonical_names
+      master_list.map { |p| p[:canonical_name].to_s }
+    end
+
+    def short_codes
+      master_list.map { |p| p[:short_code].to_s }
+    end
+
+    def names
+      master_list.map { |p| p[:name] }
+    end
+
+    def master_list
+      RAW_MASTER_LIST.map do |p|
+        p.merge(canonical_name: p[:name].parameterize(separator: "_").to_sym)
+      end
+    end
+  end
 end
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -5,4 +5,54 @@ RSpec.describe Party, type: :model do
   it { is_expected.to respond_to :name }
 
   it { is_expected.to respond_to :polls }
+
+  describe ".canonical_names" do
+    %w[
+       labour
+       liberal_democrats
+       green_party
+       conservatives
+       ukip
+       snp
+       plaid_cymru
+    ].each do |canonical_name|
+      it "includes #{canonical_name.inspect} mentioned in api docs" do
+        expect(described_class.canonical_names).to include(canonical_name)
+      end
+    end
+  end
+
+  describe ".short_codes" do
+    %w[
+      con
+      lab
+      libdem
+      brexit
+      green
+      ukip
+      snp
+      plaid
+    ].each do |short_code|
+      it "includes #{short_code.inspect} used in mapping polls info" do
+        expect(described_class.short_codes).to include(short_code)
+      end
+    end
+  end
+
+  describe ".names" do
+    [
+      "Conservatives",
+      "Green Party",
+      "Labour",
+      "Liberal Democrats",
+      "UKIP",
+      "SNP",
+      "Plaid Cymru",
+      "Brexit Party",
+    ].each do |name|
+      it "includes #{name.inspect} used in seeds scripts" do
+        expect(described_class.names).to include(name)
+      end
+    end
+  end
 end
