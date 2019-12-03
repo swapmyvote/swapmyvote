228c7e85e27b8dfe11f8dc7823dd4b64622d5d9d
baob <coder@onesandthrees.com>
2019-12-03 23:00:25 +0000

    add calculation for marginal score

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -6,4 +6,18 @@ class Poll < ApplicationRecord
              inverse_of: "polls",
              optional: true
   belongs_to :party
+
+  class << self
+    def calculate_marginal_score
+      OnsConstituency.all.each do |constituency|
+        polls = constituency.polls
+
+        polls.each do |poll|
+          party_votes = poll.votes
+          max_votes = polls.select { |p| p.id != poll.id }.map{ |p| p.votes }.max
+          poll.update(marginal_score: (max_votes - party_votes).abs)
+        end
+      end
+    end
+  end
 end
