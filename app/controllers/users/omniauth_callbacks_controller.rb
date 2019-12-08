748133a30a79a23d57e6560252826b55ae8231a9
Adam Spiers <git@adamspiers.org>
2019-12-09 03:41:11 +0400

    shorten method

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,29 +18,32 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to origin || user_path
   rescue ActiveRecord::RecordInvalid => ex
     auth = request.env["omniauth.auth"]
+    flash[:alert] = alert_for_invalid_record(ex)
+    redirect_to auth&.provider == "devise_email" ?
+                  new_user_session_path
+                : root_path + "?log_in_with=any"
+  end
 
+  def alert_for_invalid_record(ex)
     case ex.message
     when /Email can't be blank/
       alert = "Sorry, we need your email in order to continue."
       if auth&.provider == "facebook"
-        url = "https://www.facebook.com/settings?tab=applications"
-        alert <<
-          " Try <a href=\"#{url}\" target=\"_blank\">" +
-          "removing this app from your Facebook settings</a> " +
-          "then trying again.".html_safe
+        alert << " Try <a href=\"#{facebook_app_settings}\" target=\"_blank\">" +
+                 "removing this app from your Facebook settings</a> " +
+                 "then trying again.".html_safe
       end
-      flash[:alert] = alert
+      alert
     when /Email has already been taken/
-      flash[:alert] = "An account already exists with the email " \
-                      "#{auth.info.email}. " \
-                      "Please log in to that account."
+      "An account already exists with the email #{auth.info.email}. " \
+      "Please log in to that account."
     else
-      flash[:alert] = ex.message
+      ex.message
     end
+  end
 
-    redirect_to auth&.provider == "devise_email" ?
-                  new_user_session_path
-                : root_path + "?log_in_with=any"
+  def facebook_app_settings
+    "https://www.facebook.com/settings?tab=applications"
   end
 
   def new_or_existing(auth)
