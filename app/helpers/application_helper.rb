swapmyvote/swapmyvote

commit 33037e5ecfd6a30f7e83943bd168f9a76c12e555

Use Bootstrap modals instead of custom ones. Including the BS JS screws up jquery-ui a bit, fortunately it's only used for the auto-complete dropdown so we work around it.

app/views/layouts/_login.html.haml
@@ -1,51 +1,53 @@
 %span.user
-  = link_to "Already been here? Log in", "#", class: "small subdued logout", onclick: "showModal($('.js-login-modal')); return false"
-.smv-modal.js-login-modal{ style: "display: %s;" % (params[:log_in_with] ? 'inherit' : 'none') }
-  .smv-modal-dialog
-    %i.fa.fa-times.subdued.modal-close
-    .modal-header
-      %h2{ class: "h4 mb-0" } Log in
-
-    .modal-body
-      = form_with url: "#", id: "login-form", local: true do
-        - if log_in_with_facebook?
+  = link_to "Already been here? Log in", "#", class: "small subdued logout", onclick: "$('#js-login-modal').modal(); return false"
+.modal#js-login-modal{ tabindex: "-1", role: "dialog" }
+  .modal-dialog
+    .modal-content
+      .modal-header
+        %h2{ class: "h4 mb-0" } Log in
+        %button.close{ type: "button", :data => { dismiss: "modal" }, :aria => { label: "Close" } }
+          %i.fa.fa-times.subdued
+      .modal-body
+        = form_with url: "#", id: "login-form", local: true do
+          - if log_in_with_facebook?
+            %p.text-center
+              %button{ formaction: facebook_login_path, formmethod: :post,
+                      class: "btn btn-facebook btn-block" }
+                = image_tag('facebook_icon.png')
+                Log in with Facebook
+          - if log_in_with_twitter?
+            %p.text-center
+              %button{ formaction: twitter_login_path, formmethod: :post,
+                      class: "btn btn-twitter btn-block" }
+                %i.fa.fa-fw.fa-twitter
+                Log in with Twitter
+          - if log_in_with_email?
+            %p.text-center
+              %button{ formaction: new_user_session_path, formmethod: :post,
+                      class: "btn btn-dark btn-block" }
+                Log in with email
+          .form-check
+            %input.form-check-input{ type: "checkbox", id: "data-consent",
+                    name: "data-consent", required: true }
+            %label.form-check-label{ for: "data-consent" }
+              I consent to SwapMyVote processing my personal data
+          %p.text-center.small.subdued
+            We will share your voting preferences and either your email address or the
+            public link to your
+            Facebook or Twitter account with your swaps, once confirmed, so they know
+            who you are. We may send this information by email or text message.
+          %p.text-center.small.subdued
+            We will never share anything with anyone else or post to your feed
+            without your permission.
+          %p.text-center.small.subdued
+            Find out more in our
+            = link_to "Privacy Policy", privacy_path, class: "small"
+          - if ENV["DISABLE_LOG_INS"] == 'facebook'
+            %p.text-center
+              We're still waiting for our Facebook app to be approved, and we're
+              working on adding support for signing up with an email address. In
+              the meantime, you can log in with Twitter.
           %p.text-center
-            %button{ formaction: facebook_login_path, formmethod: :post,
-                    class: "btn btn-facebook btn-block" }
-              = image_tag('facebook_icon.png')
-              Log in with Facebook
-        - if log_in_with_twitter?
-          %p.text-center
-            %button{ formaction: twitter_login_path, formmethod: :post,
-                    class: "btn btn-twitter btn-block" }
-              %i.fa.fa-fw.fa-twitter
-              Log in with Twitter
-        - if log_in_with_email?
-          %p.text-center
-            %button{ formaction: new_user_session_path, formmethod: :post,
-                    class: "btn btn-dark btn-block" }
-              Log in with email
-        .form-check
-          %input.form-check-input{ type: "checkbox", id: "data-consent",
-                  name: "data-consent", required: true }
-          %label.form-check-label{ for: "data-consent" }
-            I consent to SwapMyVote processing my personal data
-        %p.text-center.small.subdued
-          We will share your voting preferences and the public link to your
-          Facebook or Twitter account with your swaps, once confirmed, so they know
-          who you are. We may send this information by email or text message.
-        %p.text-center.small.subdued
-          We will never share anything with anyone else or post to your feed
-          without your permission.
-        %p.text-center.small.subdued
-          Find out more in our
-          = link_to "Privacy Policy", privacy_path, class: "small"
-        - if ENV["DISABLE_LOG_INS"] == 'facebook'
-          %p.text-center
-            We're still waiting for our Facebook app to be approved, and we're
-            working on adding support for signing up with an email address. In
-            the meantime, you can log in with Twitter.
-        %p.text-center
-          = link_to "/faq#login", class: "small", target: "_blank" do
-            Why do I need to log in with
-            = log_in_methods + "?"
+            = link_to "/faq#login", class: "small", target: "_blank" do
+              Why do I need to log in with
+              = log_in_methods + "?"

app/assets/javascripts/autocomplete.js
@@ -1,140 +1,146 @@
-$( function() {
-    $.widget( "custom.combobox", {
-      _create: function() {
-        this.wrapper = $( "<span>" )
-          .addClass( "constituency-autocomplete" )
-          .insertAfter( this.element );
+$(function() {
+  $.widget("custom.combobox", {
+    _create: function() {
+      this.wrapper = $("<span>")
+        .addClass("constituency-autocomplete")
+        .insertAfter(this.element);
 
-        this.element.hide();
-        this._createAutocomplete();
-        this._createShowAllButton();
-      },
+      this.element.hide();
+      this._createAutocomplete();
+      this._createShowAllButton();
+    },
 
-      _createAutocomplete: function() {
-          var element = this.element,
-            selected = element.children( ":selected" ),
-            value = selected.val() ? selected.text() : "Type to select your constituency";
+    _createAutocomplete: function() {
+      var element = this.element,
+        selected = element.children(":selected"),
+        value = selected.val()
+          ? selected.text()
+          : "Type to select your constituency";
 
-        var input = this.input = $( "<input>" )
-          .appendTo( this.wrapper )
-          .val( value )
-          .attr( "title", "" )
-          .addClass( "constituency-autocomplete-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
-          .autocomplete({
-            delay: 0,
-            minLength: 0,
-            source: $.proxy( this, "_source" ),
-            classes: {
-                "ui-autocomplete": "constituency-autocomplete-select"
-            }
-          })
-          .tooltip({
-            classes: {
-              "ui-tooltip": "ui-state-highlight"
-            }
-          })
-          .focus(function () {
-            var selected = element.children( ":selected" );
-            if (!selected.val()) {
-                input.val('');
-            }
+      var input = (this.input = $("<input>")
+        .appendTo(this.wrapper)
+        .val(value)
+        .attr("title", "")
+        .addClass(
+          "constituency-autocomplete-input ui-widget ui-widget-content ui-state-default ui-corner-left"
+        )
+        .autocomplete({
+          delay: 0,
+          minLength: 0,
+          source: $.proxy(this, "_source"),
+          classes: {
+            "ui-autocomplete": "constituency-autocomplete-select"
+          }
+        })
+        .tooltip({
+          classes: {
+            "ui-tooltip": "ui-state-highlight"
+          }
+        })
+        .focus(function() {
+          var selected = element.children(":selected");
+          if (!selected.val()) {
+            input.val("");
+          }
+        }));
+
+      this._on(this.input, {
+        autocompleteselect: function(event, ui) {
+          ui.item.option.selected = true;
+          this._trigger("select", event, {
+            item: ui.item.option
           });
+        },
 
-        this._on( this.input, {
-          autocompleteselect: function( event, ui ) {
-            ui.item.option.selected = true;
-            this._trigger( "select", event, {
-              item: ui.item.option
-            });
-          },
+        autocompletechange: "_removeIfInvalid"
+      });
+    },
 
-          autocompletechange: "_removeIfInvalid"
+    _createShowAllButton: function() {
+      var input = this.input,
+        wasOpen = false;
+
+      $("<a>")
+        .attr("tabIndex", -1)
+        .appendTo(this.wrapper)
+        .addClass(
+          "ui-button ui-widget ui-button-icon-only constituency-autocomplete-toggle ui-corner-right"
+        )
+        .append("<span class='ui-button-icon ui-icon ui-icon-triangle-1-s' />")
+        .on("mousedown", function() {
+          wasOpen = input.autocomplete("widget").is(":visible");
+        })
+        .on("click", function() {
+          input.trigger("focus");
+
+          // Close if already visible
+          if (wasOpen) {
+            return;
+          }
+
+          // Pass empty string as value to search for, displaying all results
+          input.autocomplete("search", "");
         });
-      },
+    },
 
-      _createShowAllButton: function() {
-        var input = this.input,
-          wasOpen = false;
-
-        $( "<a>" )
-          .attr( "tabIndex", -1 )
-          .appendTo( this.wrapper )
-          .button({
-            icons: {
-              primary: "ui-icon-triangle-1-s"
-            },
-            text: false
-          })
-          .removeClass( "ui-corner-all" )
-          .addClass( "constituency-autocomplete-toggle ui-corner-right" )
-          .on( "mousedown", function() {
-            wasOpen = input.autocomplete( "widget" ).is( ":visible" );
-          })
-          .on( "click", function() {
-            input.trigger( "focus" );
-
-            // Close if already visible
-            if ( wasOpen ) {
-              return;
-            }
-
-            // Pass empty string as value to search for, displaying all results
-            input.autocomplete( "search", "" );
-          });
-      },
-
-      _source: function( request, response ) {
-        var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-        response( this.element.children( "option" ).map(function() {
-          var text = $( this ).text();
-          if ( this.value && ( !request.term || matcher.test(text) ) )
+    _source: function(request, response) {
+      var matcher = new RegExp(
+        $.ui.autocomplete.escapeRegex(request.term),
+        "i"
+      );
+      response(
+        this.element.children("option").map(function() {
+          var text = $(this).text();
+          if (this.value && (!request.term || matcher.test(text)))
             return {
               label: text,
               value: text,
               option: this
             };
-        }) );
-      },
+        })
+      );
+    },
 
-      _removeIfInvalid: function( event, ui ) {
-
-        // Selected an item, nothing to do
-        if ( ui.item ) {
-          return;
-        }
-
-        // Search for a match (case-insensitive)
-        var value = this.input.val(),
-          valueLowerCase = value.toLowerCase(),
-          valid = false;
-        this.element.children( "option" ).each(function() {
-          if ( $( this ).text().toLowerCase() === valueLowerCase ) {
-            this.selected = valid = true;
-            return false;
-          }
-        });
-
-        // Found a match, nothing to do
-        if ( valid ) {
-          return;
-        }
-
-        // Remove invalid value
-        this.input
-          .val( "" )
-        this.element.val( "" );
-        this._delay(function() {
-          this.input.tooltip( "close" ).attr( "title", "" );
-        }, 2500 );
-        this.input.autocomplete( "instance" ).term = "";
-      },
-
-      _destroy: function() {
-        this.wrapper.remove();
-        this.element.show();
+    _removeIfInvalid: function(event, ui) {
+      // Selected an item, nothing to do
+      if (ui.item) {
+        return;
       }
-    });
 
-    $( "#user_constituency_ons_id" ).combobox();
-  }
-);
+      // Search for a match (case-insensitive)
+      var value = this.input.val(),
+        valueLowerCase = value.toLowerCase(),
+        valid = false;
+      this.element.children("option").each(function() {
+        if (
+          $(this)
+            .text()
+            .toLowerCase() === valueLowerCase
+        ) {
+          this.selected = valid = true;
+          return false;
+        }
+      });
+
+      // Found a match, nothing to do
+      if (valid) {
+        return;
+      }
+
+      // Remove invalid value
+      this.input.val("");
+      this.element.val("");
+      this._delay(function() {
+        this.input.tooltip("close").attr("title", "");
+      }, 2500);
+      this.input.autocomplete("instance").term = "";
+    },
+
+    _destroy: function() {
+      this.wrapper.remove();
+      this.element.show();
+    }
+  });
+
+  $("#user_constituency_ons_id").combobox();
+});

app/assets/javascripts/modal.coffee
@@ -1,16 +1,0 @@
-$ () ->
-  for modal in $(".smv-modal")
-    backdrop = $("<div class='modal-backdrop'></div>")
-    $(modal).prepend(backdrop)
-    $(modal).find(".modal-close").on "click", (e) ->
-      $(".smv-modal").hide()
-      e.preventDefault()
-    backdrop.on "click", () ->
-      $(".smv-modal").hide()
-
-  $(document).keyup (e) ->
-    if e.keyCode == 27 # Escape
-      $(".smv-modal").hide()
-
-window.showModal = (element) ->
-  element.show()

app/assets/stylesheets/modal.scss
@@ -1,54 +1,0 @@
-@import "variables";
-
-$modal-width: 450px;
-$modal-margin: 12px;
-
-// FIXME
-// scss-lint:disable ColorVariable
-
-.smv-modal {
-    font-size: $base-font-size;
-
-    i.modal-close {
-        position: absolute;
-        top: 6px;
-        right: 6px;
-        cursor: pointer;
-    }
-
-    .modal-backdrop {
-        background: rgba(0, 0, 0, 0.5);
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        z-index: 10;
-    }
-
-    .smv-modal-dialog {
-        box-sizing: border-box;
-        border-radius: 5px;
-        background-color: white;
-        border: 1px solid $border-color;
-        z-index: 20;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.6);
-
-        @media screen and (min-width: $modal-width + 2 * $modal-margin) {
-            padding: 6px 24px;
-            width: $modal-width;
-            position: fixed;
-            top: 48px;
-            left: 50%;
-            margin-left: -$modal-width / 2;
-        }
-
-        @media screen and (max-width: $modal-width + 2 * $modal-margin) {
-            padding: 6px 10px;
-            font-size: $base-font-size * 0.7;
-            position: fixed;
-            left: $modal-margin;
-            right: $modal-margin;
-        }
-    }
-}

app/views/home/_swaps_open.html.haml
@@ -39,16 +39,17 @@
     preferred_party_id = $("select[name='user[preferred_party_id]']").val();
     willing_party_id = $("select[name='user[willing_party_id]']").val();
     if (preferred_party_id !== null && willing_party_id !== null) {
-      showModal($('.js-login-modal'));
+      $("#js-login-modal").modal();
     } else {
-      showModal($('.js-error-modal'));
+      $("#js-error-modal").modal();
     }
   }
 
-.smv-modal.js-error-modal{ style: "display: none;" }
-  .smv-modal-dialog
-    %i.fa.fa-times.subdued.modal-close
-    %p.text-center
-      Please choose both your preferred party and your willing party.
-    %p.text-center
-      %a.btn.btn-primary.modal-close{ href: "#" } OK
+.modal#js-error-modal{ tabindex: "-1", role: "dialog" }
+  .modal-dialog
+    .modal-content
+      .modal-body
+        Please choose both your preferred party and your willing party.
+      .modal-footer
+        %button.btn.btn-primary{ :data => { dismiss: "modal" }, :aria => { label: "Close" } }
+          Close

app/views/layouts/application.html.haml
@@ -18,7 +18,7 @@
     = favicon_link_tag(source="favicon_32.png", {sizes: "32x32"})
 
     %link{ href: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css", rel: "stylesheet", integrity: "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T", crossorigin: "anonymous" }
-    %link{ href: "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", rel: "stylesheet" }/
+    %link{ href: "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", rel: "stylesheet" }
     %script{ src: "https://www.gstatic.com/charts/loader.js", type: "text/javascript" }
     = stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track' => true
     = javascript_include_tag 'application', 'data-turbolinks-track' => true
@@ -46,3 +46,6 @@
     = yield
 
     = render partial: 'layouts/footer'
+
+    %script{ src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js", integrity: "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1", crossorigin: "anonymous" }
+    %script{ src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js", integrity: "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM", crossorigin: "anonymous" }

commit f8c4640f04c56f64673cdd91cdd6f5dd17f63c3e

fix regression of log_in_with broken by 33037e5ecfd6a

app/helpers/application_helper.rb
@@ -11,6 +11,10 @@ module ApplicationHelper
     return !current_user.nil?
   end
 
+  def display_login_modal
+    return params[:log_in_with] ? "inherit" : "none"
+  end
+
   def swapping_open?
     return true if session[:sesame] == "open" # someone said the magic word
     return !ENV["SWAPS_CLOSED"]

app/views/layouts/_login.html.haml
@@ -1,6 +1,8 @@
 %span.user
-  = link_to "Already been here? Log in", "#", class: "small subdued logout", onclick: "$('#js-login-modal').modal(); return false"
-.modal#js-login-modal{ tabindex: "-1", role: "dialog" }
+  = link_to "Already been here? Log in", "#", class: "small subdued logout",
+            onclick: "$('#js-login-modal').modal(); return false"
+.modal#js-login-modal{tabindex: "-1", role: "dialog",
+                       style: "display: %s;" % display_login_modal }
   .modal-dialog
     .modal-content
       .modal-header