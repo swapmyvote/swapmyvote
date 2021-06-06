05d1e277b8e435fb088201cd24d0b4f655d703bf
baob <coder@onesandthrees.com>
2021-06-06 11:41:54 +0100

    more succinct specs for candidate

--- spec/db/fixtures/be2021/candidate_spec.rb
+++ spec/db/fixtures/be2021/candidate_spec.rb
@@ -1,27 +1,18 @@
 require_relative "../../../../db/fixtures/be2021/candidate"
 
 RSpec.describe Db::Fixtures::Be2021::Candidate do
+  # rubocop:disable RSpec/IteratedExpectation
+  # rubocop's recommendation doesn't improve the code in this instance and it worsens the spec output
+
+  subject { described_class.new.all }
+
   describe ".all" do
-    specify { expect(subject.all).to be_an_instance_of(Array) }
-  end
+    specify { expect(subject).to be_an_instance_of(Array) }
 
-  describe "each candidate" do
-    specify do
-      subject.all.map do |candidate|
-        expect(candidate).to have_key(:name)
-      end
-    end
-
-    specify do
-      subject.all.map do |candidate|
-        expect(candidate).to have_key(:party_name)
-      end
-    end
-
-    specify do
-      subject.all.map do |candidate|
-        expect(candidate).to have_key(:constituency_ons_id)
-      end
+    describe "each candidate" do
+      specify { subject.each { |candidate| expect(candidate).to have_key(:name) } }
+      specify { subject.each { |candidate| expect(candidate).to have_key(:party_name) } }
+      specify { subject.each { |candidate| expect(candidate).to have_key(:constituency_ons_id) } }
     end
   end
 end
