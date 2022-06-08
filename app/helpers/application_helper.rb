b5ca4c1adb093a6fe7222119e776f8397f8b115a
baob <coder@onesandthrees.com>
2022-06-08 21:44:49 +0100

    fix @SwapMyVote appearing twice in twitter message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -85,11 +85,11 @@ module ApplicationHelper
     [
       "I'm using https://SwapMyVote.uk to make my vote count in the" +
       "#Wakefield or #TivertonandHoniton #byelection\\n\\n" +
-      "#SwapMyVote @SwapMyVote",
+      "#SwapMyVote",
 
       "Use https://SwapMyVote.uk to make your vote count in the " +
       "#Wakefield or #TivertonandHoniton #byelection\\n\\n" +
-      "#SwapMyVote @SwapMyVote",
+      "#SwapMyVote",
     ].sample
   end
 end
