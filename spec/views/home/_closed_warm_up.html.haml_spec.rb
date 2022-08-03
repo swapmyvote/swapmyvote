c65d8841d54ea7365897c2a38189aa8296e55fb5
baob <coder@onesandthrees.com>
2022-08-03 09:27:36 +0100

    adapt mode closed warm up for general/by election switch

--- spec/views/home/_closed_warm_up.html.haml_spec.rb
+++ spec/views/home/_closed_warm_up.html.haml_spec.rb
@@ -1,10 +1,23 @@
 require "rails_helper"
 
 RSpec.describe "home/_closed_warm_up", type: :view do
-  specify "matches snapshot" do
+  context "in a by-election" do
+    specify "matches snapshot" do
+      allow(view).to receive(:general_election?).and_return(false)
 
-    expect { render }.not_to raise_error
+      expect { render }.not_to raise_error
 
-    expect(rendered).to match_snapshot("_closed_warm_up")
+      expect(rendered).to match_snapshot("_closed_warm_up_by_election")
+    end
+  end
+
+  context "in a general election" do
+    specify "matches snapshot" do
+      allow(view).to receive(:general_election?).and_return(true)
+
+      expect { render }.not_to raise_error
+
+      expect(rendered).to match_snapshot("_closed_warm_up_general")
+    end
   end
 end
