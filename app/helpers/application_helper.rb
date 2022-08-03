0568cbb39fc60127b4d9cf7cda035ddbdef8b0b7
baob <coder@onesandthrees.com>
2022-08-03 07:23:47 +0100

    use helper method in application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -104,6 +104,10 @@ module ApplicationHelper
     "#Wakefield or #TivertonandHoniton #byelection"
   end
 
+  def election_date_and_type
+    "June 23rd 2022 by-elections"
+  end
+
   def app_taglines
     [
       "I am using Swap My Vote to make my vote count in the " +
