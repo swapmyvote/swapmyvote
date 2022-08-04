d3c5f65991b27959449f0104d3cdd5ec2146124d
baob <coder@onesandthrees.com>
2022-08-04 08:09:28 +0100

    factory for upcoming template tests

--- spec/factories/ons_constituency.rb
+++ spec/factories/ons_constituency.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :ons_constituency do
     name { "Woking" }
     ons_id { "factory-faked-ons-id-#{name.downcase}"}
+    factory :tiverton do
+      name { "Tiverton and Honiton"}
+    end
+    factory :wakefield do
+      name { "Wakefield"}
+    end
   end
 end
