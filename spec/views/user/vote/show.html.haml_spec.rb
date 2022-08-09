d308d1b888f0e87924a1053e0c7f0179c28e490a
baob <coder@onesandthrees.com>
2022-08-09 08:13:53 +0100

    add general election view specs/snapshots for views using election_hashtags

--- spec/views/user/vote/show.html.haml_spec.rb
+++ spec/views/user/vote/show.html.haml_spec.rb
@@ -1,29 +1,58 @@
 require "rails_helper"
 
 RSpec.describe "user/vote/show", type: :view do
-  context "when user has voted" do
-    specify "matches snapshot" do
-      swap = build(:swap_with_two_users)
+  before do
+    allow(OnsConstituency).to receive(:all).and_return([build(:wakefield), build(:tiverton)])
+  end
 
-      allow(swap.choosing_user).to receive(:has_voted).and_return(true)
-      assign(:user, swap.choosing_user)
+  context "when in by-election"  do
+    before do
+      allow(view).to receive(:general_election?).and_return(false)
+    end
 
-      expect { render }.not_to raise_error
+    context "when user has voted" do
+      specify "matches snapshot" do
+        swap = build(:swap_with_two_users)
 
-      expect(rendered).to match_snapshot("show_when_voted")
+        allow(swap.choosing_user).to receive(:has_voted).and_return(true)
+        assign(:user, swap.choosing_user)
+
+        expect { render }.not_to raise_error
+
+        expect(rendered).to match_snapshot("show_when_voted")
+      end
+    end
+
+    context "when user has not voted" do
+      specify "matches snapshot" do
+        swap = build(:swap_with_two_users)
+
+        allow(swap.choosing_user).to receive(:has_voted).and_return(false)
+        assign(:user, swap.choosing_user)
+
+        expect { render }.not_to raise_error
+
+        expect(rendered).to match_snapshot("show_when_not_voted")
+      end
     end
   end
 
-  context "when user has not voted" do
-    specify "matches snapshot" do
-      swap = build(:swap_with_two_users)
+  context "when in general election"  do
+    before do
+      allow(view).to receive(:general_election?).and_return(true)
+    end
 
-      allow(swap.choosing_user).to receive(:has_voted).and_return(false)
-      assign(:user, swap.choosing_user)
+    context "when user has voted" do
+      specify "matches snapshot" do
+        swap = build(:swap_with_two_users)
 
-      expect { render }.not_to raise_error
+        allow(swap.choosing_user).to receive(:has_voted).and_return(true)
+        assign(:user, swap.choosing_user)
 
-      expect(rendered).to match_snapshot("show_when_not_voted")
+        expect { render }.not_to raise_error
+
+        expect(rendered).to match_snapshot("show_when_voted_general")
+      end
     end
   end
 end
---
+++ spec/views/user_mailer/_social_media_im_using_links.html.haml_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "user_mailer/_social_media_im_using_links", type: :view do
+  context "in a by-election" do
+    specify "matches snapshot" do
+      allow(view).to receive(:general_election?).and_return(false)
+
+      expect { render }.not_to raise_error
+
+      expect(rendered).to match_snapshot("social_media_im_using_links_by_election")
+    end
+  end
+
+  context "in a general election" do
+    specify "matches snapshot" do
+      allow(view).to receive(:general_election?).and_return(true)
+
+      expect { render }.not_to raise_error
+
+      expect(rendered).to match_snapshot("social_media_im_using_links_general")
+    end
+  end
+end
