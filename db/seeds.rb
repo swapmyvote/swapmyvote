82e45ac5a933c438d4a35af699603b3a774fe1ff
baob <coder@onesandthrees.com>
2022-08-10 21:46:25 +0100

    change seed scripts to allow election type override

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
-require_relative "seeds_for_by_election"
-# require_relative "seeds_for_general_election"
+require_relative "../app/helpers/application_helper"
+
+extend ApplicationHelper
+
+puts "Override election type is #{election_type_override.inspect}"
+
+if election_type_override.nil?
+  puts "Assume by-election"
+  require_relative "seeds_for_by_election"
+elsif election_type_override == :general
+  require_relative "seeds_for_general_election"
+else
+  require_relative "seeds_for_by_election"
+end
