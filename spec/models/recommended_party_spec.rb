f5d9c070856822f51c4bfca44a6433d45ab4d017
baob <coder@onesandthrees.com>
2024-06-07 14:50:57 +0100

    RecommendedParty relates to Party

--- spec/models/recommended_party_spec.rb
+++ spec/models/recommended_party_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe RecommendedParty, type: :model do
       expect(described_class.reflections["constituency"].class_name).to eq(OnsConstituency.name)
     end
   end
+  describe "#party" do
+    specify { expect {subject.party}.not_to raise_error }
+
+    it "is a relation to Party" do
+      expect(described_class.reflections["party"].class_name).to eq(Party.name)
+    end
+  end
 end
