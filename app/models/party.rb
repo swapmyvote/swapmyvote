529e0d42f3fb6b6fa5c36e3ad6a33ac29aa94ce4
Steve Baxter <steve@kuamka.com>
2024-06-23 18:47:03 +0200

    Add party labels to prediction chart
    This adds a short name to the prediction chart.
    
    Closes #825.

--- app/models/party.rb
+++ app/models/party.rb
@@ -5,19 +5,19 @@ class Party < ApplicationRecord
            dependent: :destroy
 
   REFERENCE_DATA = {
-    lab: { name: "Labour", color: "#DC241f", ge_default: true },
-    libdem: { name: "Liberal Democrats", color: "#FFB602", ge_default: true },
-    green: { name: "Green", color: "#6AB023", ge_default: true },
-    con: { name: "Conservatives", color: "#0087DC", ge_default: true },
-    ukip: { name: "UKIP", color: "#70147A" },
-    snp: { name: "SNP", color: "#FFF95D", ge_default: true },
-    plaid: { name: "Plaid Cymru", color: "#008142", ge_default: true },
-    brexit: { name: "Brexit", color: "#5bc0de" },
+    lab: { name: "Labour", color: "#DC241f", short_name: "Lab", ge_default: true },
+    libdem: { name: "Liberal Democrats", short_name: "LD", color: "#FFB602", ge_default: true },
+    green: { name: "Green", color: "#6AB023", short_name: "Green", ge_default: true },
+    con: { name: "Conservatives", color: "#0087DC", short_name: "Con", ge_default: true },
+    ukip: { name: "UKIP", short_name: "UKIP", color: "#70147A" },
+    snp: { name: "SNP", short_name: "SNP", color: "#FFF95D", ge_default: true },
+    plaid: { name: "Plaid Cymru", short_name: "PC", color: "#008142", ge_default: true },
+    brexit: { name: "Brexit", short_name: "Brexit", color: "#5bc0de" },
     breakthrough: { name: "Breakthrough" },
     for_britain: { name: "For Britain" },
     freedom_alliance: { name: "Freedom Alliance" },
-    independent: { name: "Independent" },
-    reform: { name: "Reform", color: "#5bc0de", ge_default: true },
+    independent: { name: "Independent", short_name: "Ind" },
+    reform: { name: "Reform", short_name: "Ref", color: "#5bc0de", ge_default: true },
     rejoin_eu: { name: "Rejoin eu" },
     workers: { name: "Workers" },
     yorkshire: { name: "Yorkshire" },
@@ -31,6 +31,12 @@ class Party < ApplicationRecord
       return ons_id[0] == "S"
     end
     return true
+  end
+
+  def short_name
+    short = smv_code ? REFERENCE_DATA[smv_code.to_sym][:short_name] : nil
+    short ||= name.downcase.gsub(/[aeiou]/, "")[0..2].titleize
+    short
   end
 
   class << self
