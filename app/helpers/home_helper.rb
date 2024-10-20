316946c6bdb11c38cf5685b2270439869e4c21db
baob <coder@onesandthrees.com>
2024-10-20 11:42:51 +0100

    change the flow to constituency first
    This is a combination of 7 commits.
    
    HACKING towards constituency first - step 1-6
    
    unsuccessful in getting the hidden field really hidden. I suspect some CSS skills required

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,28 @@
 module HomeHelper
+  def pre_login_flow
+    return session["pre_login_flow"] || {}
+  end
+
+  def pre_login_constituency_form_complete
+    !!pre_login_flow["constituency_form_complete"]
+  end
+
+  def pre_login_candidates_form_complete
+    !!pre_login_flow["candidates_form_complete"]
+  end
+
+  def add_pre_login_constituency(ons_id)
+    session["pre_populate"] ||= {}
+    session["pre_login_flow"] ||= {}
+    session["pre_populate"]["constituency_ons_id"] = ons_id
+    session["pre_login_flow"]["constituency_form_complete"] = true
+  end
+
+  def add_pre_login_parties(user)
+    session["pre_populate"] ||= {}
+    session["pre_login_flow"] ||= {}
+    session["pre_populate"]["willing_party_id"] = user["willing_party_id"]
+    session["pre_populate"]["preferred_party_id"] = user["preferred_party_id"]
+    session["pre_login_flow"]["parties_form_complete"] = true
+  end
 end
