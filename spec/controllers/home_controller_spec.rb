7c665926811f53ce275cef859ba478fd58d42659
baob <coder@onesandthrees.com>
2024-11-02 17:32:40 +0000

    fix pre-population specs for home page
    now fully addresses comment on commit 3a94e853

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -16,11 +16,12 @@ RSpec.describe HomeController, type: :controller do
   end
 
   def test_renders_home_page(params = {})
-    create(:ons_constituency, name: "Burkshire")
+    constituency = create(:ons_constituency, name: "Burkshire", ons_id: "burk1")
     get :index, params: params
     expect(subject).to render_template(:index)
     expect(assigns(:constituencies)).to all(be_a(OnsConstituency))
     expect(assigns(:constituencies).count).to be >= 1
+    return constituency
   end
 
   context "home page: when not logged in" do
@@ -39,32 +40,28 @@ RSpec.describe HomeController, type: :controller do
         .to change {session[:sesame] }.to(nil)
     end
 
-    # it "prepopulates preferred party from session" do
-    #   slug = "liberal_democrats"
-    #   session["pre_populate"] = { "preferred_party_name" => slug }
-    #   party = test_renders_home_page
-    #   expect(assigns(:preferred_party_id)).to eq(party.id)
-    # end
-
-    # it "prepopulates willing party from session" do
-    #   slug = "liberal_democrats"
-    #   session["pre_populate"] = { "willing_party_name" => slug }
-    #   party = test_renders_home_page
-    #   expect(assigns(:willing_party_id)).to eq(party.id)
-    # end
-
-    it "doesn't prepopulate an unrecognised preferred party from session" do
-      slug = "green"
-      session["pre_populate"] = { "preferred_party_name" => slug }
-      test_renders_home_page
-      expect(assigns(:preferred_party_id)).to be_nil
+    it "prepopulates constituency from session ons_id" do
+      session["pre_populate"] = { "constituency_ons_id" => "burk1" }
+      constituency = test_renders_home_page
+      expect(assigns(:default_constituency_ons_id)).to eq(constituency.ons_id)
     end
 
-    it "doesn't prepopulate an unrecognised willing party from session" do
-      slug = "green"
-      session["pre_populate"] = { "willing_party_name" => slug }
+    it "doesn't prepopulate an unrecognised constituency from session ons_id" do
+      session["pre_populate"] = { "constituency_ons_id" => "you_will_never_find_me" }
       test_renders_home_page
-      expect(assigns(:willing_party_id)).to be_nil
+      expect(assigns(:default_constituency_ons_id)).to be_nil
+    end
+
+    it "prepopulates constituency from session name" do
+      session["pre_populate"] = { "constituency_name" => "Burkshire" }
+      constituency = test_renders_home_page
+      expect(assigns(:default_constituency_ons_id)).to eq(constituency.ons_id)
+    end
+
+    it "doesn't prepopulate an unrecognised constituency from session name" do
+      session["pre_populate"] = { "constituency_name" => "you_will_never_find_me" }
+      test_renders_home_page
+      expect(assigns(:default_constituency_ons_id)).to be_nil
     end
   end
 
