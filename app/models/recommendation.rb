1787c4807e42d43990a65659a70bea94500f5b09
baob <coder@onesandthrees.com>
2024-11-03 15:43:29 +0000

    issue-1025: simplify models/recommendation now we no longer use LFB aggregator - 3

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -29,42 +29,4 @@ class Recommendation < ApplicationRecord
       print "*" # to signify remove with heart/any
     end
   end
-
-  class << self
-    def refresh_from_json(progress: false)
-      json = LivefrombrexitRecommendationsJson.new
-
-      with_sanity_checks(progress: progress) do |timestamp|
-        json.each do |rec_as_hash|
-          rec = Recommendation.find_or_initialize_by(rec_as_hash.slice("site", "constituency_ons_id"))
-          rec.text = rec_as_hash["recommendation"]
-          rec.link = rec_as_hash["link"]
-          rec.updated_at = timestamp
-          rec.save!
-          print "."  if progress
-        end
-      end
-    end
-
-    def with_sanity_checks(progress: false, &block)
-      timestamp = DateTime.now
-      before_count = Recommendation.count
-      puts "#{before_count} records before update" if progress
-
-      block.call(timestamp)
-
-      puts if progress
-      puts "#{Recommendation.count} records after update"  if progress
-      select_untouched = Recommendation.where.not(updated_at: timestamp)
-      untouched_count = select_untouched.count
-      puts "#{untouched_count} records untouched"  if progress
-
-      if untouched_count > before_count / 3
-        raise "something went very wrong here, more than a third of records not included in this update?"
-      end
-
-      select_untouched.delete_all
-      puts "#{Recommendation.count} records after deleting untouched"  if progress
-    end
-  end
 end
