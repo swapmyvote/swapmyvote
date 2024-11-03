5c40a187ee671c02df0a7c9789e0556ad8a56365
baob <coder@onesandthrees.com>
2024-11-03 15:35:50 +0000

    issue-1025: simplify models/recommendation now we no longer use LFB aggregator - 2

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -6,26 +6,11 @@ class Recommendation < ApplicationRecord
              primary_key: "ons_id",
              foreign_key: "constituency_ons_id"
 
-  LFB_REFERENCE_DATA = {
-    lab:    { style: "background-color: rgba(220, 36, 31, 0.4); color: rgb(134, 0, 0);", short_name: "Lab" },
-    libdem: { style: "background-color: rgba(250, 166, 26, 0.4); color: rgb(166, 97, 0);", short_name: "LD" },
-    green:  { style: "background-color: rgba(106, 176, 35, 0.4); color: rgb(24, 105, 0);", short_name: "Green" },
-    con:    { style: "background-color: rgba(0, 135, 220, 0.4); color: rgb(0, 70, 146);", short_name: "Con" },
-    ukip:   { style: "background-color: rgba(112, 20, 122, 0.4); color: rgb(36, 0, 38);", short_name: "UKIP" },
-    snp:    { style: "background-color: rgba(240, 202, 62, 0.4); color: rgb(159, 130, 0);", short_name: "SNP" },
-    plaid:  { style: "background-color: rgba(0, 129, 66, 0.4); color: rgb(0, 61, 3);", short_name: "Plaid" },
-    brexit: { style: "background-color: rgba(18, 182, 207, 0.4); color: rgb(0, 111, 134);", short_name: "BXP" }
-  }
-
   LFB_DEFAULT_STYLE = "background-color: rgba(204, 204, 204, 0.4); color: rgb(132, 132, 132);"
 
   def party_short_code_from_text
     canonical_text_sym = text.downcase.parameterize(separator: "_").to_sym
     return canonical_text_sym if Party::REFERENCE_DATA.keys.include?(canonical_text_sym)
-  end
-
-  def party_style_from_text
-    return party_attributes_from_text[:style]
   end
 
   def update_parties(parties)
@@ -43,11 +28,6 @@ class Recommendation < ApplicationRecord
       party.delete
       print "*" # to signify remove with heart/any
     end
-  end
-
-  private def party_attributes_from_text
-    short_code = party_short_code_from_text
-    return LFB_REFERENCE_DATA[short_code] || { style: LFB_DEFAULT_STYLE }
   end
 
   class << self
--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -8,18 +8,4 @@ RSpec.describe Recommendation, type: :model do
       expect(described_class.reflections["constituency"].class_name).to eq(OnsConstituency.name)
     end
   end
-
-  describe "#party_style_from_text" do
-    Party.smv_codes.each do |smv_code|
-      describe "for party \"#{smv_code.titlecase}\"" do
-        specify { expect(described_class.new(text: smv_code).party_style_from_text).not_to be_nil }
-        specify { expect(described_class.new(text: smv_code).party_style_from_text).not_to eq("") }
-      end
-    end
-
-    describe "for text not matching a party" do
-      specify { expect(described_class.new(text: "combustible edison").party_style_from_text).not_to be_nil }
-      specify { expect(described_class.new(text: "combustible edison").party_style_from_text).not_to eq("") }
-    end
-  end
 end
